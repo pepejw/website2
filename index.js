2dd41abe0f095dec06851eb8728689a8a34551b8
Pepe Wightmore <pepejw@proton.me>
2024-12-15 19:33:55 +0000

    more things on web

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function main(){
+    
+}
+function changePage(page){
+    let activePage = document.querySelectorAll(".active");
+    activePage.forEach(element => {element.classList.remove("active");
+    document.getElementById(page).classList.add("active");
+    });
+}
+function changeLang(lang) {
+    
+}
