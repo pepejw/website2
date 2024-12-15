3b6bd20115a7195d090259a9dfcb33618e062c05
Pepe Wightmore <pepejw@proton.me>
2024-12-15 23:55:37 +0000

    idk what i did but things

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ function changePage(page){
     document.getElementById(page).classList.add("active");
     });
 }
-function changeLang(lang) {
-    
+function changeLang() {
+    switch (document.getElementById("lang").innerText){
+        case "english":
+            document.getElementById("title").innerText = "pepe's site"
+            document.getElementById("home").innerText = "home"
+            document.getElementById("about").innerText = "about"
+            document.getElementById("socials").innerText = "socials"
+            document.getElementById("lang").innerText = "toki pona";
+            break;
+        case "toki pona":
+            document.getElementById("title").innerText = "lipu jan pepe"
+            document.getElementById("home").innerText = "tomo"
+            document.getElementById("about").innerText = "sona"
+            document.getElementById("socials").innerText = "lipu kulupu"
+            document.getElementById("lang").innerText = "english";
+            break;
+    }
 }
+
+let about_en = `
+<h2>Hi, I'm Pepe</h2>
+
+
+`
